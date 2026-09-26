1db2222d740e95b9283723d9e895a5a34e50b2fb
prcssy <2023301693@pampangastateu.edu.ph>
2026-09-26 19:25:16 +0800

    feat: fix waiver

--- src/student/DashboardPage.jsx
+++ src/student/DashboardPage.jsx
@@ -2319,6 +2319,14 @@ function PlayerRegistration({ onBack }) {
                     )
                   }
                 </label>
+                <a
+                  className="reg-template-link"
+                  href="/templates/parental-consent-form.docx"
+                  download="Parental Consent Form - Santa Rita College of Pampanga.docx"
+                >
+                  <svg width="12" height="12" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M5 20h14v-2H5v2zM19 9h-4V3H9v6H5l7 7 7-7z"/></svg>
+                  Download consent form template
+                </a>
               </Field>
 
               <Field label="Message">
